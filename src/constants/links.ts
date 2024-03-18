3d41bd1a00229677ade62d4e3d2f4b9326f758de
TLOWAC <leechanghun55@gmail.com>
2024-03-18 12:15:59 +0900

    chore: profile link 데이터 추가

---
+++ src/constants/links.ts
@@ -0,0 +1,16 @@
+export const LINK_GROUP = [
+  {
+    "target": "GitHub",
+    "url": "https://github.com/TLOWAC",
+  }, {
+    "target": "LinkedIn",
+    "url": "https://www.linkedin.com/in/chang-hun-lee-380725216/",
+  },
+  {
+    "target": "Instagram",
+    "url": "https://www.instagram.com/tlowac.dev/",
+  }, {
+    "target": "Blog Posts",
+    "url": "https://helicopter55.tistory.com/82",
+  }
+] as const;
